5c714623b664f413bfefa65f760757cd754da71f
Takuma HANATANI <mail@potato4d.me>
2020-01-21 18:17:24 +0900

    feat: Support global scope

--- src/yomu.ts
+++ src/yomu.ts
@@ -2,10 +2,22 @@
 import meow from 'meow'
 import { exec } from './main'
 
-const cli = meow(`
+const cli = meow(
+  `
   Usage
     $ yomu <package-name>
-`, {
-})
+
+  Global Scope
+    $ yomu -g <package-name>
+`,
+  {
+    flags: {
+      global: {
+        type: 'boolean',
+        alias: 'g'
+      }
+    }
+  }
+)
 
 exec(cli.input[0], cli.flags)
